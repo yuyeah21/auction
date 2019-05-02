e5fc59c178a7171977af5e10995e49c0710d5b3b
“yuyeah21” <yuyeah21@gmail.com>
2019-05-02 01:32:25 -0500

    登録更新機能完成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,29 @@
 class ItemsController < ApplicationController
   def show
-    @item = Item.find_by(params[:id])
+    @item = Item.find(params[:id])
   end
   def index
     @items = Item.all
+  end
+  def new
+    @item = Item.new
+  end
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to item_path(@item)
+  end
+  def edit
+    @item = Item.find(params[:id])
+  end
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to @item
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :price, :seller, :description, :email, :image_url)
   end
 end
